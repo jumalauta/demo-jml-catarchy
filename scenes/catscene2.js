e536d9e5089fa8d36d4c69d98db58e6007ed0a42
saul-l <sauli.lehtinen@gmail.com>
2025-09-13 10:43:15 +0300

    skateboard v1

--- scenes/catscene2.js
+++ scenes/catscene2.js
@@ -1,24 +1,24 @@
 Demo.prototype.sceneCat2 = function () {
-    this.setScene('cat2');
+  this.setScene('cat2');
 
 
 
-      this.loader.addAnimation([{
-      object:{
-          "name":"scenes/aurinkolasikissa.obj"
-        }
-      ,position:[{
-        x:0,
-        y:-.5,
-        z:-10
-      }]
-      ,scale:[{"uniform3d":2}]
-      ,angle:[{             
-        degreesX:()=>(getSceneTimeFromStart()%window.beat)*-150+45 ,      
-        degreesY:()=>-90+45*Math.sin(5*getSceneTimeFromStart()) ,    
-        degreesZ:()=>0 ,               
-      }]
-    }]);
+  this.loader.addAnimation([{
+    object: {
+      "name": "scenes/aurinkolasikissa.obj"
+    }
+    , position: [{
+      x: 0,
+      y: -.5,
+      z: -10
+    }]
+    , scale: [{ "uniform3d": 2 }]
+    , angle: [{
+      degreesX: () => (getSceneTimeFromStart() % window.beat) * -150 + 45,
+      degreesY: () => -90 + 45 * Math.sin(5 * getSceneTimeFromStart()),
+      degreesZ: () => 0,
+    }]
+  }]);
 
-    this.basicText({text:"Purr",x:0,y:0,scale:6.0, start:0, duration: 100});
+  this.basicText({ text: "Purr", x: 0, y: 0, scale: 6.0, start: 0, duration: 100 });
 }
--- scenes/catscene9.js
+++ scenes/catscene9.js
@@ -1,16 +1,56 @@
 Demo.prototype.sceneCat9 = function () {
     this.setScene('cat9');
 
-      this.loader.addAnimation([{
-      image: { name: 'scenes/pikka1.png'},
-      perspective: '3d',
-      position:[{
-        x:()=>Math.sin(getSceneTimeFromStart()*2),
-        y:()=>.5*Math.cos(getSceneTimeFromStart()*2)+getSceneTimeFromStart()%window.beat,
-        z:0}],
-      scale:[{"uniform3d":()=>Sync.get('Foreground:scale2')}],
-      color:[{"r":1.0,"g":0.1,"b":0.1,"a":1.0}],
-    }]);
+  this.loader.addAnimation([{
+    id: "skateboard",
+    object: {
+      "name": "scenes/skateboard.obj"
+    }
+    , position: [{
+      x: () => .3 * Math.sin(8 * getSceneTimeFromStart()),
+      y: -3,
+      z: -10
+    }]
+    , scale: [{ "uniform3d": 1 }]
+    , angle: [{
 
+      degreesY: () => -90 + 15 * Math.sin(2 * getSceneTimeFromStart()),
+    }]
+  }]);
+
+  this.loader.addAnimation([{
+    id: "catbody",
+    parent: "skateboard",
+    object: {
+      "name": "scenes/catbody_normal.obj"
+    }
+    , position: [{
+      x: 0,
+      y: () => .5 + Sync.get('Cat:Jump') * (-.5 + 1.3 + Math.sin((getSceneTimeFromStart() * 16))),
+      z: 0
+    }]
+    , scale: [{ "uniform3d": 1 }]
+    , angle: [{
+    }]
+  }]);
+
+
+  this.loader.addAnimation([{
+    parent: "catbody",
+    object: {
+      "name": "scenes/aurinkolasikissa.obj"
+    }
+    , position: [{
+      x: 0,
+      y: 3,
+      z: 2
+    }]
+    , scale: [{ "uniform3d": 1 }]
+    , angle: [{
+      degreesX: () => (getSceneTimeFromStart() % window.beat) * -150 + 45,
+      degreesY: () => -90 + 45 * Math.sin(5 * getSceneTimeFromStart()),
+      degreesZ: () => 0,
+    }]
+  }]);
     this.basicText({text:"Meow",x:0,y:0,scale:6.0, start:0, duration: 100});
 }
