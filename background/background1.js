311ad0e58c80324d0d33a85d3af6bbb6c7c06251
Mika Rautio <1129758+mrautio@users.noreply.github.com>
2025-09-13 13:48:38 +0000

    colorcycling

--- background/background1.js
+++ background/background1.js
@@ -20,7 +20,44 @@ Demo.prototype.sceneBackground1 = function () {
         scale: [{ uniform2d: 2.3 }],
         color: [{
           r: 1, g: 1, b: 1
-      }]
+      }],
+      shader: {
+      name: 'background/colorcycle.fs',
+      variable: [
+        {"name":"centerize","value":[()=>0.0]},
+        {"name":"shiftHue", "value":[()=>{
+          const time = getSceneTimeFromStart();
+          const bpm = 120;
+          const changePerBeat = 1.0/8.0;
+          const currentBeat = (time/60.0)*bpm;
+
+          if (time > 117.2) {
+            return (time*6.0)%1.0;
+          }
+          //return (time/60.0)*bpm*changePerBeat % 1.0;
+          if (currentBeat % 4.0 < 2.0) {
+            return (time/60.0)*bpm*changePerBeat % 1.0;
+          } else {
+            return 0.0;//1.0 + 0.5 * (1.0 - (currentBeat % 1.0)) * 2.0;
+          }
+      }]},
+        {"name":"shiftSaturation", "value":[()=>1.0]},
+        {"name":"shiftValue", "value":[()=>{
+
+          const time = getSceneTimeFromStart();
+          if (time >= 12 && time < 16) {
+            return 0;
+          }
+          const bpm = 120;
+          const currentBeat = (time/60.0)*bpm;
+          if (currentBeat % 3.0 < 0.2) {
+            return 1.0 + 0.5 * ((currentBeat % 1.0) / 0.2) * 2.0;
+          } else {
+            return 0.0;//1.0 + 0.5 * (1.0 - (currentBeat % 1.0)) * 2.0;
+          }
+      }]}
+      ]
+    }
       }
     ]);
 }
